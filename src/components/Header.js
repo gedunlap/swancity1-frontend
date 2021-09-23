026a7185d928822f2dbe22ff4c1304d863608076
gedunlap <g.e.dunlap90@gmail.com>
2021-09-23 15:04:45 -0400

    sassy changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom'
 export default function Header() {
     return (
         <nav className="nav">
+            <Link to='/'>
+                <img src="https://i.imgur.com/vaF3qy4.png" alt="Swan City Modern" />
+            </Link>
             <Link to='/' className="navitem" style={{textDecoration:'none'}}>
                 <div>Home</div>
             </Link>
