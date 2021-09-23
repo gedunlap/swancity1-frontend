daba27cd9e29743f5275264bce54e39c99ea7df8
gedunlap <g.e.dunlap90@gmail.com>
2021-09-23 15:26:09 -0400

    index displays

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Index() {
-    return (
-        <div>
-            Index
-        </div>
-    )
+export default function Index(props) {
+    
+    const loaded = () => {
+        return props.products.map((product) => (
+            <div key={product.id} className="product">
+                <Link to={`/products/${product._id}`}><h1>{product.name}</h1></Link>
+                <img src={product.image} alt={product.name} />
+                <h3>{product.manufacturer}</h3>
+            </div>
+        ))
+    }
+
+    const loading = () => {
+        return <h1>Loading...</h1>
+    }
+
+    return props.products ? loaded() : loading()
 }
