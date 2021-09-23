0ce1171084733f0f4bb32b9762d969c5daf11df2
gedunlap <g.e.dunlap90@gmail.com>
2021-09-23 14:48:31 -0400

    navbar and some sass

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <nav className="nav">
+            <Link to='/' className="navitem" style={{textDecoration:'none'}}>
+                <div>Home</div>
+            </Link>
+            <Link to='/about' className="navitem" style={{textDecoration:'none'}}>
+                <div>About</div>
+            </Link>
+            <Link to='/product' className="navitem" style={{textDecoration:'none'}}>
+                <div>Products</div>
+            </Link>
+        </nav>
     )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Index from '../pages/Index'
+import Show from '../pages/Show'
 
-export default function Main() {
+export default function Main(props) {
     return (
-        <div>
-            Main
-        </div>
+        <main className="main">
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/product">
+                    <Index />
+                </Route>
+                <Route 
+                    path="/product/:id"
+                    render={(rp) => (
+                        <Show {...rp} />
+                    )}
+                />
+            </Switch>
+        </main>
     )
 }
