e778fdb6ba265eedccdc05111f77db28390e35f2
gedunlap <g.e.dunlap90@gmail.com>
2021-09-23 16:00:04 -0400

    working on that index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -7,7 +7,7 @@ export default function Index(props) {
     const loaded = () => {
         return props.products.map((product) => (
             <div key={product.id} className="product">
-                <Link to={`/products/${product._id}`}><h1>{product.name}</h1></Link>
+                <Link to={`/products/${product._id}`} style={{textDecoration:'none'}}><h1>{product.name}</h1></Link>
                 <img src={product.image} alt={product.name} />
                 <h3>{product.manufacturer}</h3>
             </div>
